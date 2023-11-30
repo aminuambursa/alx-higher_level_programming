e4dbb3440abc6582aaf49e641dcad44e8a325e83
Aminu Ambursa <aminumusaambursa123@gmail.com>
2023-11-30 14:40:43 -0800

    Tasks

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
-# Import the function add from the file add_0.py
-from add_0 import add
+if __name__ == "__main__":
+    from add_0 import add
 
-# Define variables a and b
-a = 1
-b = 2
-
-# Call the add function and print the result using string formatting
-print(f"{a} + {b} = {add(a, b)}")
+    a = 1
+    b = 2
+    print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
